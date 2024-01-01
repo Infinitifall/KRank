5e3ecacb2deb3135d30b65839a77df34c34d0ce6
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2024-01-01 12:11:37 +0530

    add latest data, update readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# KRank
+
+***[Go to website](https://infinitifall.github.io/KRank/)***
+
+Krunker.io leaderboard data is used to estimate player/clan rank.
+
+
+## Screenshots
+
+![Screenshot of the KRank webpage](screenshot.jpeg)
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function calculate_player_rank() {
     let player_level = document.getElementById("player_level").value;
-    rank = Math.pow(player_level / 353.341654925471, 1 / (-0.116559433021637));
+    rank = Math.pow(player_level / 375.014687647882, 1 / (-0.120203030642183));
     rank = Math.round(rank);
     document.getElementById("player_rank").innerHTML = "Player's approximate rank is " + rank;
 }
 
 function calculate_clan_rank() {
     let clan_xp = document.getElementById("clan_xp").value;
-    rank = Math.pow(clan_xp / 9765518203.36893, 1 / (-0.803866367443068));
+    rank = Math.pow(clan_xp / 10064755961.5884, 1 / (-0.783051223137246));
     rank = Math.round(rank);
     document.getElementById("clan_rank").innerHTML = "Clan's approximate rank is " + rank;
 }
